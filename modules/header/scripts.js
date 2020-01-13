04bde56ccce5fef9525de475d8b4b38cac12e6db
David Sergeev <d_sergeevi@cu.edu.ge>
2020-01-14 02:58:43 +0400

    header + responsive

---
+++ modules/header/scripts.js
@@ -0,0 +1,29 @@
+let showHideHamburger = () => {
+    $(".hamburger").on("click", function() {
+        $(".stick").eq(0).toggleClass("one");
+        $(".stick").eq(1).toggleClass("two");
+        $(".stick").eq(2).toggleClass("three");
+        $('.r-menu').slideToggle();
+        if($('body').css("overflow-y") == "auto") {
+            $('body').css("overflow-y", "hidden");  
+        } else {
+            $('body').css("overflow-y", "auto");
+        }
+    });
+}
+
+
+$(window).on("load", function() {
+    $('.header-body.owl-carousel').owlCarousel({
+        loop: true,
+        stagePadding: 0,
+        nav: false,
+        dots: true, 
+        autoWidth:false,
+        singleItem: true,
+        margin: 0,
+        items: 1,
+    });
+
+    showHideHamburger();
+});
